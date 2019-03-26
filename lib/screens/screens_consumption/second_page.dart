2c913f5c96894ef30255b4ac712b0a465aa84dd0
Dibaloke Chanda <dibaloke66@gmail.com>
2019-03-27 02:43:41 +0600

    Screens Implemented Except Notification and second,third page of Automation

--- lib/screens/screens_consumption/second_page.dart
+++ lib/screens/screens_consumption/second_page.dart
@@ -1,14 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
  
 class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Container(
       child: new Center(
-        child: new Text('Third Page',
-        style: new TextStyle(fontSize: 25.0, color: Colors.teal),
-      ),
+        child:SafeArea(child: TimeSeriesBar())
       ),
     );
   }
+}
+
+//Attempt
+
+class TimeSeriesBar extends StatefulWidget {
+
+  
+  @override
+  _TimeSeriesBarState createState() => _TimeSeriesBarState();
+}
+
+class _TimeSeriesBarState extends State<TimeSeriesBar> {
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: charts.TimeSeriesChart(_createSampleData()),
+      
+    );
+  }
+}
+
+List<charts.Series<TimeSeriesSales, DateTime>> _createSampleData() {
+    var data = [
+      new TimeSeriesSales(new DateTime(2017, 9, 1), 5),
+      new TimeSeriesSales(new DateTime(2017, 9, 2), 5),
+      new TimeSeriesSales(new DateTime(2017, 9, 3), 25),
+      new TimeSeriesSales(new DateTime(2017, 9, 4), 100),
+      new TimeSeriesSales(new DateTime(2017, 9, 5), 75),
+      new TimeSeriesSales(new DateTime(2017, 9, 6), 88),
+      new TimeSeriesSales(new DateTime(2017, 9, 7), 65),
+      new TimeSeriesSales(new DateTime(2017, 9, 8), 91),
+      new TimeSeriesSales(new DateTime(2017, 9, 9), 100),
+      new TimeSeriesSales(new DateTime(2017, 9, 10), 111),
+      new TimeSeriesSales(new DateTime(2017, 9, 11), 90),
+      new TimeSeriesSales(new DateTime(2017, 9, 12), 50),
+      new TimeSeriesSales(new DateTime(2017, 9, 13), 40),
+      new TimeSeriesSales(new DateTime(2017, 9, 14), 30),
+      new TimeSeriesSales(new DateTime(2017, 9, 15), 40),
+      new TimeSeriesSales(new DateTime(2017, 9, 16), 50),
+      new TimeSeriesSales(new DateTime(2017, 9, 17), 30),
+      new TimeSeriesSales(new DateTime(2017, 9, 18), 35),
+      new TimeSeriesSales(new DateTime(2017, 9, 19), 40),
+      new TimeSeriesSales(new DateTime(2017, 9, 20), 32),
+      new TimeSeriesSales(new DateTime(2017, 9, 21), 31),
+    ];
+
+    return [
+      new charts.Series<TimeSeriesSales, DateTime>(
+        id: 'Sales',
+        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+        domainFn: (TimeSeriesSales sales, _) => sales.time,
+        measureFn: (TimeSeriesSales sales, _) => sales.sales,
+        data: data,
+      )
+    ];
+  }
+
+
+
+
+
+
+
+/// Sample time series data type.
+class TimeSeriesSales {
+  final DateTime time;
+  final int sales;
+
+  TimeSeriesSales(this.time, this.sales);
 }
--- lib/screens/screens_consumption/third_page.dart
+++ lib/screens/screens_consumption/third_page.dart
@@ -1,14 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
  
 class ThirdPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Container(
-      child: new Center(
-        child: new Text('Third Page',
-        style: new TextStyle(fontSize: 25.0, color: Colors.teal),
-      ),
-      ),
+      child: SafeArea(child: MyGroupedBarChart()),
     );
   }
+}
+
+class MyGroupedBarChart extends StatefulWidget {
+  @override
+  _MyGroupedBarChartState createState() => _MyGroupedBarChartState();
+}
+
+class _MyGroupedBarChartState extends State<MyGroupedBarChart> {
+  @override
+  Widget build(BuildContext context) {
+     return new charts.BarChart(
+      _createSampleData(),
+    
+      
+    );
+      
+    
+  }
+
+  List<charts.Series<OrdinalSales, String>> _createSampleData() {
+    final desktopSalesData = [
+      new OrdinalSales('Bedroom', 5),
+      new OrdinalSales('Kitchen', 25),
+      new OrdinalSales('Washroom', 100),
+    
+    ];
+
+    final tableSalesData = [
+      new OrdinalSales('Bedroom', 25),
+      new OrdinalSales('Kitchen', 50),
+      new OrdinalSales('Washroom', 10),
+      
+    ];
+
+    final mobileSalesData = [
+      new OrdinalSales('Bedroom', 10),
+      new OrdinalSales('Kitchen', 15),
+      new OrdinalSales('Washroom', 50),
+     
+    ];
+
+    return [
+      new charts.Series<OrdinalSales, String>(
+        id: 'Desktop',
+        domainFn: (OrdinalSales sales, _) => sales.year,
+        measureFn: (OrdinalSales sales, _) => sales.sales,
+        data: desktopSalesData,
+      ),
+      new charts.Series<OrdinalSales, String>(
+        id: 'Tablet',
+        domainFn: (OrdinalSales sales, _) => sales.year,
+        measureFn: (OrdinalSales sales, _) => sales.sales,
+        data: tableSalesData,
+      ),
+      new charts.Series<OrdinalSales, String>(
+        id: 'Mobile',
+        domainFn: (OrdinalSales sales, _) => sales.year,
+        measureFn: (OrdinalSales sales, _) => sales.sales,
+        data: mobileSalesData,
+      ),
+    ];
+  }
+}
+class OrdinalSales {
+  final String year;
+  final int sales;
+
+  OrdinalSales(this.year, this.sales);
 }
