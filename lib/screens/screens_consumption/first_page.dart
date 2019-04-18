a795de18a48510b96a41145263a7d4b07307d584
Dibaloke Chanda <dibaloke66@gmail.com>
2019-04-19 03:11:21 +0600

    Basic Chart with data pulled from firestore

--- lib/screens/screens_consumption/first_page.dart
+++ lib/screens/screens_consumption/first_page.dart
@@ -1,93 +1,118 @@
 import 'package:flutter/material.dart';
-import 'package:pie_chart/pie_chart.dart';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
- var firestore_refernce_cfl_bedroom=Firestore.instance.collection('/Loads/CFL Light/Rooms').document('Bedroom');
-
-     getquery() async {
-    var a= await firestore_refernce_cfl_bedroom.get();
-    int i=0;
-    while (true) {
-      if(a[i.toString()]==null){
-        break;
-      }
-      else{
-        print(a[i.toString()]);
-      }
-      i++;
+var firestore_refernce_cfl_bedroom =
+    Firestore.instance.collection('/Loads/CFL Light/Rooms').document('Bedroom');
+//*Pull Data For Chart
+Future<List<double>> getquery() async {
+  List<double> data_list = [];
+  var a = await firestore_refernce_cfl_bedroom.get();
+  int i = 0;
+  while (true) {
+    if (a[i.toString()] == null) {
+      break;
+    } else {
+      // print(a[i.toString()]);
     }
-    
-    
+    data_list.add(a[i.toString()]);
+    i++;
   }
+  // print(data_list);
+  return data_list;
+}
+
+var data = [
+  Sales("Sun", 1, Colors.red),
+  Sales("Mon", 0, Colors.green),
+  Sales("Tue", 0, Colors.yellow),
+  Sales("Wed", 0, Colors.pink),
+  Sales("Thu", 0, Colors.purple),
+  Sales("Fri", 0, Colors.brown),
+  Sales("Sat", 0, Colors.orange),
+];
+
+//*Pull Data For Chart
 class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      
-      body:Chart() ,
+      body: SafeArea(child: Chart()),
     );
-    
   }
 }
 
-
 class Chart extends StatefulWidget {
+  Chart({Key key, this.title}) : super(key: key);
+
+  final String title;
+
   @override
   _ChartState createState() => _ChartState();
 }
 
 class _ChartState extends State<Chart> {
-
-
-  
-bool toggle = false;
-  Map<String, double> dataMap = new Map();
- double lightenergy=0;
- double fanenergy=0;
- double otherloadenergy=0;
-  
-  @override
-  void initState() {
-    super.initState();
-    dataMap.putIfAbsent("Light", () => lightenergy);
-    dataMap.putIfAbsent("Fan", () => 3);
-    dataMap.putIfAbsent("Other Loads", () => 2);
-    
-  }
-
   @override
   Widget build(BuildContext context) {
-   getquery();
-    return PieChart(
-      dataMap: dataMap,
-      legendFontColor: Colors.blueGrey[900],
-      legendFontSize: 14.0,
-      legendFontWeight: FontWeight.w500,
-      animationDuration: Duration(milliseconds: 800),
-      chartLegendSpacing: 32.0,
-      chartRadius: MediaQuery
-          .of(context)
-          .size
-          .width / 2.7,
-      showChartValuesInPercentage: true,
-      showChartValues: true,
-      chartValuesColor: Colors.blueGrey[900].withOpacity(0.9),
-      );
-  }
-    void togglePieChart() {
-    setState(() {
-      toggle = !toggle;
-    });
-  }
+    var series = [
+      charts.Series(
+          domainFn: (Sales sales, _) => sales.day,
+          measureFn: (Sales sales, _) => sales.sold,
+          colorFn: (Sales sales, _) => sales.color,
+          id: 'Sales',
+          data: data)
+    ];
+    var chart = charts.PieChart(
+      series,
+      defaultRenderer: charts.ArcRendererConfig(
+          arcRendererDecorators: [charts.ArcLabelDecorator()], arcWidth: 100),
+      animate: true,
+    );
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.refresh),
+        onPressed: () {
+          getquery().then((List value) {
+            data = [
+              Sales("Sun", value[0], Colors.red),
+              Sales("Mon", value[1], Colors.green),
+              Sales("Tue", value[2], Colors.yellow),
+            ];
 
-   queryValues() async {
-   var total = 0.0;
+            setState(() {
+              data = [
+                Sales("Sun", value[0], Colors.red),
+                Sales("Mon", value[1], Colors.green),
+                Sales("Tue", value[2], Colors.yellow),
+              ];
+            });
+          });
+        },
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(2.0),
+        child: Column(
+          children: <Widget>[
+            Text(
+              "Load Consumption",
+              style: TextStyle(fontSize: 30),
+            ),
+            SizedBox(height: 500, child: chart),
+          ],
+        ),
+      ),
+    );
+  }
+  
 
-     
-  var docs = firestore_refernce_cfl_bedroom.snapshots();
-  
-  
-  print(docs);
 }
-  
+
+class Sales {
+  final String day;
+  double sold;
+  final charts.Color color;
+  Sales(this.day, this.sold, Color color)
+      : this.color = charts.Color(
+            r: color.red, g: color.green, b: color.blue, a: color.alpha);
 }
